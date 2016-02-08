6d7fba41f0e0dd2f43a3da328d3d06138ae2394b
蒋思聪 <1211037588@qq.com>
2016-02-08 16:17:42 -0600

    Initial Commit

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by patron on 16/2/8.
+//  Copyright © 2016年 Kevin Sicong Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HomeWork1Tests/HomeWork1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HomeWork1Tests.swift
+//  HomeWork1Tests
+//
+//  Created by patron on 16/2/8.
+//  Copyright © 2016年 Kevin Sicong Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import HomeWork1
+
+class HomeWork1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
