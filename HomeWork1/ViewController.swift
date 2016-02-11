7ca597a428c56dbebf1d7175f50abe86c90fda65
蒋思聪 <1211037588@qq.com>
2016-02-10 23:23:08 -0600

    First commit, working with tab view controller

--- HomeWork1/ViewController.swift
+++ HomeWork1/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPushed(sender: AnyObject) {
+        self.performSegueWithIdentifier("toNavController", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
