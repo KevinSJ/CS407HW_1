b46d58b01f8f18534740095bf722775f2808b3b3
Kevin S Jiang <sjiang42@wisc.edu>
2016-02-14 11:56:05 -0600

    Final view display problem

--- HomeWork1/displayFinalResultViewController.swift
+++ HomeWork1/displayFinalResultViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class displayFinalResultViewController: UIViewController {
     var answers:[String] = [String]()
-
+    
+    @IBOutlet weak var userAnswer1: UILabel!
+    @IBOutlet weak var userAnswer2: UILabel!
+    @IBOutlet weak var userAnswer3: UILabel!
+    @IBOutlet weak var userAnswer4: UILabel!
+    
+    @IBOutlet weak var correctAnswer1: UILabel!
+    @IBOutlet weak var correctAnswer2: UILabel!
+    @IBOutlet weak var correctAnswer3: UILabel!
+    @IBOutlet weak var correctAnswer4: UILabel!
+    
+    
+    @IBOutlet weak var finalScore: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkResults(answers)
-        // Do any additional setup after loading the view.
+        let usrAnswersL:[UILabel]=[userAnswer1,userAnswer2,userAnswer3,userAnswer4]
+        let correctAnswersL:[UILabel]=[correctAnswer1,correctAnswer2,correctAnswer3,correctAnswer4]
+        let correctAnswers = ["Abraham Lincoln","02-12-1806","Gettysburg","54"]
+        let correct = compareResults(self.answers, correctAnswers: correctAnswers)
+        
+        fillUsrAnswers(usrAnswersL, answers:self.answers)
+        fillCorrectAnswers(correctAnswersL, answers:correctAnswers)
+        
+        finalScore.text = "You get "+String(correct/4 * 100) + String(" %")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +44,31 @@ class displayFinalResultViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func checkResults(answers:[String]) -> Int{
-        var i:Int = 1
-        for answer in answers{
-            print("Answers" + answer)
-            i++
+    func compareResults(answers:[String],correctAnswers:[String])->Int{
+        var count:Int = 0
+        for(index,element) in answers.enumerate(){
+            if (element == correctAnswers[index]){
+                count = count + 1
+            }
         }
-        return i
+        return count
     }
-
+    
+    func fillUsrAnswers(usrAnswers:[UILabel],answers:[String]){
+        for(index,element) in answers.enumerate(){
+                usrAnswers[index].text = element
+        }
+    }
+    
+    func fillCorrectAnswers(correctAnswers:[UILabel],answers:[String]){
+        for(index,element) in answers.enumerate(){
+            correctAnswers[index].text = element
+        }
+    }
+    
+    @IBAction func btnRestartPush(sender: AnyObject) {
+        self.performSegueWithIdentifier("backToHomeScreen", sender: self)
+    }
     /*
     // MARK: - Navigation
 
